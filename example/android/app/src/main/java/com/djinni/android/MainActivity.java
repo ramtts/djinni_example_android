356d85daa8a16d66b0fd711ad642590bf617ccd1
Nguyen Van The <the@floatingcube.com>
2014-10-30 21:30:34 +0700

    modify MainActivity class

--- example/android/app/src/main/java/com/djinni/android/MainActivity.java
+++ example/android/app/src/main/java/com/djinni/android/MainActivity.java
@@ -8,29 +8,29 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+    private EditText text;
+    private SortItems sortItemsInterface;
+    private TextboxListener textboxListener;
+
+    static {
+        System.loadLibrary("textsort_jni");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (EditText) findViewById(R.id.editText);
+        textboxListener = new TextboxListenerImpl(text);
+        // Call JNI to initiate the SortItems object from the given textboxListener and translate to Java
+        sortItemsInterface = SortItems.createWithListener(textboxListener);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public void sort(View view) {
+        String str = text.getText().toString();
+        ArrayList<String> items = new ArrayList<String>(Arrays.asList(str.split("\n")));
+        ItemList itemList = new ItemList(items);
+        sortItemsInterface.sort(itemList);
     }
 }
